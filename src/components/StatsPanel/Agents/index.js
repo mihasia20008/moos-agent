3f5b9de9b10bf23f64d8b62f4874000ef4db3cf8
Alexander Mikhaylov <aleksandr.mikhaylov@onetwotrip.com>
2019-02-25 00:25:41 +0300

    add html and css fixes

--- src/components/StatsPanel/Agents/index.js
+++ src/components/StatsPanel/Agents/index.js
@@ -13,11 +13,29 @@ const AgentsStatsPanel = () => {
             <div className={cx('fr-stats-panel__item')}>
                 <span className={cx('fr-stats-panel__title')}>Сделки</span>
                 <div className={cx('fr-stats-panel__stats')}>
-                    <span className={cx('fr-stats-panel__stats-value')}>192 821</span>
-                    <span className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--purple')}>1241</span>
-                    <span className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--yellow')}>9124</span>
-                    <span className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--red')}>37 888</span>
-                    <span className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--green')}>87 125</span>
+                    <span className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--bold')}>
+                        192 821
+                    </span>
+                    <span
+                        className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--bold fr-stats-panel__stats-value--purple')}
+                    >
+                        1241
+                    </span>
+                    <span
+                        className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--bold fr-stats-panel__stats-value--yellow')}
+                    >
+                        9124
+                    </span>
+                    <span
+                        className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--bold fr-stats-panel__stats-value--red')}
+                    >
+                        37 888
+                    </span>
+                    <span
+                        className={cx('fr-stats-panel__stats-value fr-stats-panel__stats-value--bold fr-stats-panel__stats-value--green')}
+                    >
+                        87 125
+                    </span>
                 </div>
             </div>
             <div className={cx('fr-stats-panel__item')}>
